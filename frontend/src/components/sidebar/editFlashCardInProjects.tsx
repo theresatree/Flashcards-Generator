8aa7bc84eb1f0702b779fd386114a5cf0cc73d40
Hayes <1a1.nghoeping@gmail.com>
2025-06-28 00:30:24 +0800

    edited file name

--- frontend/src/components/sidebar/editFlashCardInProjects.tsx
+++ frontend/src/components/sidebar/editFlashCardInProjects.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { motion } from "motion/react";
 import RenderFlashcards from "../renderFlashcards";
@@ -65,4 +65,5 @@ function EditFlashcardsInProjects(){
 }
 
 export default EditFlashcardsInProjects;
+
 
